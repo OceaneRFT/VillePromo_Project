b2bc26c284bca56a3809a375fcf0d37981d03835
oceane <oceane.ruffato@outlook.fr>
2021-01-18 15:01:08 +0100

    view product

--- resources/views/productpage.blade.php
+++ resources/views/productpage.blade.php
@@ -1,156 +1,81 @@
 @extends('layouts.public.layoutPublic')
 
 @section('content')
-<section class="container product_picture">
-    <div class="ui two column grid container">
-        <div class="column">
-            <div class="ui segment product_item">
-                <div class="ui two column grid container">
-                    <div class="six column product_picture1">
-                        <div class="column">
-                            <div class="ui segment picture_product">
-                                <img>
-                            </div>
-                        </div>
-                        <div class="column">
-                            <div class="ui segment picture_product">
-                                <img>
-                            </div>
-                        </div>
-                        <div class="column">
-                            <div class="ui segment picture_product">
-                                <img>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="six column product_picture2">
-                        <div class="column">
-                            <div class="ui segment picture_product2">
-                                <img>
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="col-6">
+                <div class="card mb-4 box-shadow">
+                    <img class="card-img-top" src="produits/goonies.jpg" alt="Card image cap">
+                </div>
+            </div>
+            <div class="col-6">
+                <h1 class="jumbotron-heading">T-Shirt Goonies</h1>
+                <h5>29.00 €</h5>
+                <p class="lead text-muted">Cinoque aime choco! consectetur adipisicing elit. Dignissimos dolore eaque earum
+                    eos ex, exercitationem facilis magni maiores maxime natus neque odit quo quod recusandae tempora unde ut
+                    veritatis vero!</p>
+                <hr>
+                <label for="size">Choisissez votre taille</label>
+                <select name="size" id="size" class="form-control">
+                    <option value="xs">XS</option>
+                    <option value="s">S</option>
+                    <option value="m">M</option>
+                    <option value="l">L</option>
+                    <option value="xl">XL</option>
+                    <option value="xxl">XXL</option>
+                </select>
+                <p>
+                    <a href="#" class="btn btn-cart my-2 btn-block"><i class="fas fa-shopping-cart"></i> Ajouter au
+                        Panier</a>
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="album py-5 bg-light">
+        <div class="container">
+            <div class="row">
+                <h3>Vous aimerez aussi :</h3>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img src="produits/hulk.jpg" class="card-img-top img-fluid" alt="Responsive image">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-end">
+                                <div class="btn-group">
+                                    <a href="#" class="btn btn-sm btn-outline-secondary"><i class="fas fa-eye"></i></a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div class="column">
-            <div class="ui segment product_item">
-                <div class="ui product_list">
-                    <div class="ui bulleted list">
-                        <div class="item list_product">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officiis tempora fuga neque cumque hic! Nostrum est minus ipsum laborum facilis libero, quae explicabo ipsa, architecto reprehenderit nesciunt laboriosam molestiae.</div>
-                        <div class="item list_product">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
-                        <div class="item list_product">
-                            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img src="produits/krusty_simpsons.jpg" class="card-img-top img-fluid" alt="Responsive image">
+
+                        <div class="card-body">
+                            <div class="d-flex justify-content-end">
+                                <div class="btn-group">
+                                    <a href="#" class="btn btn-sm btn-outline-secondary"><i class="fas fa-eye"></i></a>
+                                </div>
+                            </div>
                         </div>
-                        <div class="item list_product">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img src="produits/star_trek_kirk.jpg" class="card-img-top img-fluid" alt="Responsive image">
+                        <div class="card-body">
+                            <div class="d-flex justify-content-end">
+                                <div class="btn-group">
+                                    <a href="#" class="btn btn-sm btn-outline-secondary"><i class="fas fa-eye"></i></a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</section>
-
-<section class="description_product">
-    <div class="ui segment">
-        <img class="ui centered medium image" src="/images/wireframe/image.png">
-        <p>Te eum doming eirmod, nominati pertinacia argumentum ad his. Ex eam alia facete scriptorem, est autem aliquip
-            detraxit at. Usu ocurreret referrentur at, cu epicurei appellantur vix. Cum ea laoreet recteque electram, eos
-            choro alterum definiebas in. Vim dolorum definiebas an. Mei ex natum rebum iisque.</p>
-        <p>Audiam quaerendum eu sea, pro omittam definiebas ex. Te est latine definitiones. Quot wisi nulla ex duo. Vis sint
-            solet expetenda ne, his te phaedrum referrentur consectetuer. Id vix fabulas oporteat, ei quo vide phaedrum, vim
-            vivendum maiestatis in.</p>
-        <p>Te eum doming eirmod, nominati pertinacia argumentum ad his. Ex eam alia facete scriptorem, est autem aliquip
-            detraxit at. Usu ocurreret referrentur at, cu epicurei appellantur vix. Cum ea laoreet recteque electram, eos
-            choro alterum definiebas in. Vim dolorum definiebas an. Mei ex natum rebum iisque.</p>
-        <p>Audiam quaerendum eu sea, pro omittam definiebas ex. Te est latine definitiones. Quot wisi nulla ex duo. Vis sint
-            solet expetenda ne, his te phaedrum referrentur consectetuer. Id vix fabulas oporteat, ei quo vide phaedrum, vim
-            vivendum maiestatis in.</p>
-        <img class="ui small centered image" src="/images/wireframe/image.png">
-        <p>Te eum doming eirmod, nominati pertinacia argumentum ad his. Ex eam alia facete scriptorem, est autem aliquip
-            detraxit at. Usu ocurreret referrentur at, cu epicurei appellantur vix. Cum ea laoreet recteque electram, eos
-            choro alterum definiebas in. Vim dolorum definiebas an. Mei ex natum rebum iisque.</p>
-        <p>Audiam quaerendum eu sea, pro omittam definiebas ex. Te est latine definitiones. Quot wisi nulla ex duo. Vis sint
-            solet expetenda ne, his te phaedrum referrentur consectetuer. Id vix fabulas oporteat, ei quo vide phaedrum, vim
-            vivendum maiestatis in.</p>
-        <p>Te eum doming eirmod, nominati pertinacia argumentum ad his. Ex eam alia facete scriptorem, est autem aliquip
-            detraxit at. Usu ocurreret referrentur at, cu epicurei appellantur vix. Cum ea laoreet recteque electram, eos
-            choro alterum definiebas in. Vim dolorum definiebas an. Mei ex natum rebum iisque.</p>
-        <p>Audiam quaerendum eu sea, pro omittam definiebas ex. Te est latine definitiones. Quot wisi nulla ex duo. Vis sint
-            solet expetenda ne, his te phaedrum referrentur consectetuer. Id vix fabulas oporteat, ei quo vide phaedrum, vim
-            vivendum maiestatis in.</p>
-        <p>Eu quo homero blandit intellegebat. Incorrupte consequuntur mei id. Mei ut facer dolores adolescens, no illum
-            aperiri quo, usu odio brute at. Qui te porro electram, ea dico facete utroque quo. Populo quodsi te eam, wisi
-            everti eos ex, eum elitr altera utamur at. Quodsi convenire mnesarchum eu per, quas minimum postulant per id.
-        </p>
-    </div>
-</section>
-<section class="container comment">
-    <div class="ui comments">
-        <div class="comment">
-            <a class="avatar">
-                <img src="">
-            </a>
-            <div class="content">
-                <a class="author author_comment">Pseudo 1</a>
-                <div class="metadata">
-                    <div class="date date_comment"></div>
-                    <div class="rating rating_comment">
-                        <i class="star icon"></i>
-                        5 Avis
-                    </div>
-                </div>
-                <div class="text text_comment">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id modi eligendi cum libero voluptatibus? Accusamus quia laborum, ullam molestiae eligendi vero veritatis aliquid iusto molestias sint, porro perspiciatis deleniti aperiam.
-                </div>
-            </div>
-        </div>
-        <div class="comment">
-            <a class="avatar">
-                <img src="">
-            </a>
-            <div class="content">
-                <a class="author author_comment">Pseudo 1</a>
-                <div class="metadata">
-                    <div class="date date_comment"></div>
-                    <div class="rating rating_comment">
-                        <i class="star icon"></i>
-                        3 Avis
-                    </div>
-                </div>
-                <div class="text text_comment">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit sed fugiat repellendus unde, enim error tempora inventore at repellat perspiciatis, architecto aliquam facilis. Tempore laborum quas soluta eius cupiditate ex!
-                </div>
-            </div>
-        </div>
-    </div>
-
-</section>
-
-
-<div class="ui divider"></div>
-
-<section class="container featured_category">
-    <div class="ui four column grid container">
-        <div class="column">
-            <div class="ui segment featuredcategory_item">
-                <img>
-            </div>
-        </div>
-        <div class="column">
-            <div class="ui segment featuredcategory_item">
-                <img>
-            </div>
-        </div>
-        <div class="column">
-            <div class="ui segment featuredcategory_item">
-                <img>
-            </div>
-        </div>
-        <div class="column">
-            <div class="ui segment featuredcategory_item">
-                <img>
-            </div>
-        </div>
-    </div>
-</section>
 @endsection
